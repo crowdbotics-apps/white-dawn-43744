4da79cb0644224d11bd4640c11c763c22c35b345
Crowdbotics <team@crowdbotics.com>
2023-10-03 21:24:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-dawn-43744",
+  url: "https://white-dawn-43744.botics.co",
+  api: "https://white-dawn-43744.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
